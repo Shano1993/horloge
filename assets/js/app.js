d6e50a34628814177f7bb22b24e270aab3cc499d
Shano1993 <stefan.hanotiau@gmail.com>
2021-12-12 12:25:11 +0100

    horloge

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,22 @@
-let instant = new Date();
-let second = document.getElementById('second');
-let minute = document.getElementById('minute');
-let hour = document.getElementById('hour');
+let seconds = document.getElementById('second');
+let minutes = document.getElementById('minute');
+let hours = document.getElementById('hour');
 
+function clock() {
+    setInterval(function () {
+        let instant = new Date();
+        const second = instant.getSeconds();
+        const secondDeg = ((second/60)*360);
+        seconds.style.transform = `rotate(${secondDeg}deg)`;
 
+        const minute = instant.getMinutes();
+        const minuteDeg = ((minute/60)*360) + ((second/60)*6) + 90;
+        minutes.style.transform = `rotate(${minuteDeg}deg)`;
+
+        const hour = instant.getHours();
+        const hourDeg = ((hour/12)*360) + ((minute/60)*30) + 90;
+        hours.style.transform = `rotate(${hourDeg}deg)`;
+    }, 1000);
+}
+
+clock();
